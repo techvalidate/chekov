933f0d2a994b32c8525e8e6aa4b8a9c338e31ad4
Jordan McKible <jordan@mckible.com>
2011-09-14 17:05:42 -0700

    Append checks on newly created elements.

--- app/models/element.rb
+++ app/models/element.rb
@@ -3,7 +3,15 @@ class Element < ActiveRecord::Base
   belongs_to :story
   
   has_many :checks, :dependent=>:destroy
-    
+  
+  after_create :propagate_checks
+  def propagate_checks
+    story.suites.each do |suite|
+      check = suite.checks.build element: self
+      check.save
+    end
+  end
+  
   validates_presence_of :description
   
 end
--- test/unit/element_test.rb
+++ test/unit/element_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class ElementTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'add checks to existing suites on append to story' do
+    element = stories(:create_user).elements.build :description=>'new element'
+    element.save
+    assert_equal 3, stories(:create_user).reload.elements.count
+    assert_equal 3, suites(:jordan_create_user).checks.count
+  end
 end
