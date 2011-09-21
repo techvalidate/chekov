fae14fc60b4d3e1b800307480ea22de6c9f13903
Jordan McKible <jordan@mckible.com>
2011-09-20 17:16:55 -0700

    View everyone's assignments.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,11 @@ class UsersController < ApplicationController
   
   # GET /users/:id
   def show
+    if params[:user_id]
+      @user = User.find params[:user_id]
+    else
+      @user = current_user
+    end
   end
 
 end
