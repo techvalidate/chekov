a03b3f0312356bc0a6f537a66f1d702a6191182a
Jordan McKible <jordan@mckible.com>
2015-08-27 11:46:11 -0700

    serve_static_file config.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Chekov::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
+  config.serve_static_file = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -39,7 +39,7 @@ Chekov::Application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
   config.action_dispatch.x_sendfile_header = nil # Heroku
-  
+
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
